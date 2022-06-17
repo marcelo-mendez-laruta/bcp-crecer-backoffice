69b3b7c0e4e92dc811b9edbbc45ed1c7e9c7d353
Marcelo Carlos Mendez Laruta <MMendez@bancred.com.bo>
2022-06-17 16:33:24 -0400

    fix pwa

--- vue.config.js
+++ vue.config.js
@@ -1,35 +1,4 @@
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   transpileDependencies: ["vuetify"],
-  pwa: {
-    manifestOptions: {
-      name: "BCP Crecer",
-      short_name: "BCP Crecer",
-      start_url: "./",
-      display: "standalone",
-      theme_color: "#000000",
-      icons: [
-        {
-          src: "./favicon.svg",
-          sizes: "512x512",
-          type: "image/svg+xml",
-          purpose: "any maskable",
-        },
-      ],
-    },
-
-    themeColor: "#4DBA87",
-    msTileColor: "#000000",
-    appleMobileWebAppCapable: "yes",
-    appleMobileWebAppStatusBarStyle: "black",
-    iconPaths: {
-      maskicon: null,
-      favicon32: "./favicon32.png",
-      favicon16: "./favicon16.png",
-      appleTouchIcon: null,
-      msTileImage: null,
-    },
-    // configure the workbox plugin
-    workboxPluginMode: "GenerateSW",
-  },
 });
